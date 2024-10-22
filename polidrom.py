6ff3d19cc938e5f5512234d477aabef733d01176
Shamsiddin Kamoliddinov <160200088+bekfurr@users.noreply.github.com>
2024-10-22 12:03:30 +0500

    Add files via upload

---
+++ polidrom.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+def polidrom():
+    soz = entry1.get()
+    n = len(soz)
+    b = ""
+    for i in range(n-1, -1, -1):
+        b += soz[i]
+    if soz == b:
+        label2 = Label(win, text="Soz polidrom", foreground="White", background="Green"  )
+        label2.grid(row=2, column=0, columnspan=2)
+    else:
+        label2 = Label(win, text="Soz polidrom emas", foreground="White", background="Red"  )
+        label2.grid(row=2, column=0, columnspan=2)
+        
+win = Tk()
+
+win.title("Polidrom Tekshirish")
+
+label1 = Label(win, text="So'zni kiriting:")
+
+label1.grid(row=0, column=0)
+
+entry1 = Entry(win)
+
+entry1.grid(row=0, column=1)
+
+qiymat = Text(win, height=5, width=30)
+
+
+
+tugma = Button(win, text="Tekshirish", command=polidrom)
+tugma.grid(row=1, column=0, columnspan=2)
+
+win.mainloop()
+    
+
+
+
+
+
+
